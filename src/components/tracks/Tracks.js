a9172b4506b59c1f0116927b77540b32dfb9abf9
Алексей Бельченков <u608110@gmail.com>
2019-08-08 17:28:01 +0400

    Add Spinner to project

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { Consumer } from '../../context';
+import Spinner from "../layout/Spinner";
 
 class Tracks extends Component {
     render() {
@@ -8,8 +9,13 @@ class Tracks extends Component {
             <div>
                 <Consumer>
                     {value => {
-                        console.log(value);
-                        return <h1>Tracks</h1>
+                        const { track_list } = value;
+
+                        if (track_list === undefined || track_list.length === 0) {
+                            return <Spinner />;
+                        } else {
+                            return <h1>Tracks</h1>
+                        }
                     }}
                 </Consumer>
             </div>
