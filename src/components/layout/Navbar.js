9287e76347acfad2edfe2f1fc8575d00764341fc
Алексей Бельченков <u608110@gmail.com>
2019-08-08 17:58:26 +0400

    Output Track Item

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-dark bg-info mb-5">
+        <nav className="navbar navbar-dark bg-primary mb-5">
             <span className="navbar-brand mb-0 h1 mx-auto">
                 <img src="https://img.icons8.com/doodle/48/000000/treble-clef.png" alt="logo" />
                 LyricFinder
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import { Consumer } from '../../context';
 import Spinner from "../layout/Spinner";
+import Track from "./Track";
 
 class Tracks extends Component {
     render() {
@@ -9,12 +10,27 @@ class Tracks extends Component {
             <div>
                 <Consumer>
                     {value => {
-                        const { track_list } = value;
+                        const { track_list, heading } = value;
 
                         if (track_list === undefined || track_list.length === 0) {
                             return <Spinner />;
                         } else {
-                            return <h1>Tracks</h1>
+                            return (
+                                <Fragment>
+                                    <h2 className="text-center mb-4">
+                                        <img src="https://img.icons8.com/plasticine/50/000000/music.png" />
+                                        { heading }
+                                    </h2>
+                                    <div className="row">
+                                        {track_list.map(item => (
+                                            <Track
+                                                track={item.track}
+                                                key={item.track.track_id}
+                                            />
+                                        ))}
+                                    </div>
+                                </Fragment>
+                            );
                         }
                     }}
                 </Consumer>
