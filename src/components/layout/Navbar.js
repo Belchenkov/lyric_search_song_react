d7623d606d82f22d819d41a0e12a139c630d7a2d
Алексей Бельченков <u608110@gmail.com>
2019-08-08 16:26:09 +0400

    Add Provider & send state

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-info mb-5">
             <span className="navbar-brand mb-0 h1 mx-auto">
-                <img src="https://img.icons8.com/doodle/48/000000/treble-clef.png" />
+                <img src="https://img.icons8.com/doodle/48/000000/treble-clef.png" alt="logo" />
                 LyricFinder
-                <img src="https://img.icons8.com/doodle/48/000000/sound-cloud-.png" />
+                <img src="https://img.icons8.com/doodle/48/000000/sound-cloud-.png" alt="logo" />
             </span>
         </nav>
     );
